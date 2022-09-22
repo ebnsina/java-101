89212b77ebbf259ca28a0b5bcf29d5cca20e2207
Ebn Sina <ebnsina.me@gmail.com>
2022-09-22 21:57:41 +0600

    code organize

--- Basics/Hello.java
+++ Basics/Hello.java
@@ -1,8 +1,8 @@
-/**
- * Hello
- */
+package Basics;
+
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        System.out.println("Hello, World!");
     }
+
 }
